8c4427868cecd17e106718c9e7f807e9db851d0c
James Spencer <thisjustosay@gmail.com>
2017-03-09 22:32:31 -0800

    close bracket issue

--- www/HappieCamera.js
+++ www/HappieCamera.js
@@ -3,10 +3,11 @@ module.exports = {
         var opts = {quality:resolutionInt}
         //signature - js success callback, js fail callback, native code class name, native code initialize method
         cordova.exec(success, failure, "HappieCamera", "openCamera", [opts]);
-    }
+    }    
+}
     
 module.exports.generateThumbnail = function (name, success, failure) {
         var opts = {name:name}
         //signature - js success callback, js fail callback, native code class name, native code initialize method
         cordova.exec(success, failure, "HappieCamera", "generateThumbnail", [opts]);
-    }
+}
