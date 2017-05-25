f1f6a623ccfe9b437ebb6104c8f5c9ab44051edb
James Spencer <thisjustosay@gmail.com>
2017-05-24 21:17:33 -0700

    use single export

--- www/HappieCamera.js
+++ www/HappieCamera.js
@@ -3,11 +3,10 @@ module.exports = {
         var opts = {quality:resolutionInt};
         //signature - js success callback, js fail callback, native code class name, native code initialize method
         cordova.exec(success, failure, "HappieCamera", "openCamera", [opts]);
-    }    
-};
-    
-module.exports.generateThumbnail = function (name, success, failure) {
+    },
+    generateThumbnail: function (name, success, failure) {
         var opts = {name:name};
         //signature - js success callback, js fail callback, native code class name, native code initialize method
         cordova.exec(success, failure, "HappieCamera", "generateThumbnail", [opts]);
+    }
 };
